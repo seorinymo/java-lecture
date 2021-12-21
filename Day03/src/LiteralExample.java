a298664c72d56ea7f77cf61382d7defa46690ecf
seorinymo <sjh9377a@naver.com>
2021-12-21 21:21:38 +0900

    리터럴 상수연습

---
+++ Day03/src/LiteralExample.java
@@ -0,0 +1,21 @@
+
+public class LiteralExample {
+
+	public static void main(String[] args) {
+		
+		// 리터럴 (이스케이프 시퀀스)
+		String desc = "\"이종석\"\n 집에 가서\n 잠이나 \t자";
+		System.out.println(desc);
+		
+		//"이종석" 집에 가서 잠이나 자 -> "\"이종석\" 집에 가서 잠이나 자";
+		//"이종석"  \n (뉴라인)
+		//집에 가서  \n
+		//잠이나	자  \t
+		
+		// \r \n (뉴라인)
+		
+		
+		
+		
+	}
+}
